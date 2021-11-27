7cb7f5a7211d67acc1095623007e9d011e9c76a9
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-27 16:15:34 +0800

    not valid to invalid

--- passwordValidator.py
+++ passwordValidator.py
@@ -5,27 +5,27 @@ while True:
   is_valid = False
 
   if (len(user_input) > 15):
-    print("Not valid ! Total characters should be greater than 15")
+    print("Invalid password! Total characters should be greater than 15")
     continue
 
   elif not re.search("[A-Z]",user_input):
-    print("Not valid ! It should contain atleast one capital letter [A-Z]")
+    print("Invalid password! It should contain atleast one capital letter [A-Z]")
     continue
 
   elif not re.search("[a-z]",user_input):
-    print("Not valid ! It should contain atleast one lowercase letter [a-z]")
+    print("Invalid password! It should contain atleast one lowercase letter [a-z]")
     continue
 
   elif not re.search("[1-9]",user_input):
-    print("Not valid ! It should contain one number between [1-9]")
+    print("Invalid password! It should contain one number between [1-9]")
     continue
 
   elif not re.search("[!@#$%^&*()_+ etc]",user_input):
-    print("Not valid ! It should contain at least one special character in [!@#$%^&*()_+ etc]")
+    print("Invalid password! It should contain at least one special character in [!@#$%^&*()_+ etc]")
     continue
 
   elif re.search("[\s]",user_input):
-    print("Not valid ! It should not contain any space")
+    print("Invalid password! It should not contain any space")
     continue
 
   else:
@@ -33,4 +33,4 @@ while True:
     break
 
 if(is_valid):
-  print("Password is valid")
+  print("Your password is valid!")
